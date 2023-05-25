ecf669e0f44033a46e67bffb1e9aa3760d13c734
Golukumar-2007 <105867776+Golukumar-2007@users.noreply.github.com>
2023-05-25 19:53:32 +0530

    Add files via upload

---
+++ role-a-dice.py
@@ -0,0 +1,54 @@
+import random
+
+res="y"
+
+
+while res == "y":
+
+    roll= random.randint(1,6)
+
+    if roll==1:
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("\n")
+    if roll==2:
+        print("[0    ]")
+        print("[     ]")
+        print("[    0]")
+        print("\n")
+    if roll==3:
+        print("[0    ]")
+        print("[  0  ]")
+        print("[    0]")
+        print("\n")
+    if roll==4:
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+        print("\n")
+    if roll==5:
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+        print("\n")
+    if roll==6:
+        print("[0   0]")
+        print("[0   0]")
+        print("[0   0]")
+        print("\n")
+
+        res = input("Press 'y' to roll dice and press 'n' to exit: ")
+        print("\n")
+
+
+# mam par wo pelhe 3 output de raha hai fir input mang raha hai 
+
+
+
+# line no 4.
+# line no 5.
+# line no 9.
+# line no 10.
+
+
